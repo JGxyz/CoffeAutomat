0d0f7b1c5f4072cbf904ff2ef3502ef1e7161669
rafal <rafal.lisak1@gmail.com>
2019-05-08 13:08:35 +0200

    Nie wiem czy to jest CoinBack czy CoinBank

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Automate automate = new Automate();
+        System.out.println("\n 1 - insert coin \n 2 - prepare coffee \n 3 - receive coffee \n 4 -  return coin \n 5 - get coin bank \n");
 
 while(true) {
     Scanner scan = new Scanner(System.in);
